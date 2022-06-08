3d138386c0550fcf9a7aba3553e7e221a458b6ea
RWGregory <91198091+RWGregory@users.noreply.github.com>
2022-06-07 21:10:59 -0400

    initial build of homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
+import '../styles/styles.scss'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
+
   return <Component {...pageProps} />
 }
 
