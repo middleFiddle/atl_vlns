ec50e4f78a89569016615bb5c1214bac606857e6
Ryan Gregory <ryan.middleFiddle@gmail.com>
2022-07-09 18:40:55 -0400

    instrument gallery prototype with prisma

--- pages/Sales.tsx
+++ pages/Sales.tsx
@@ -1,7 +1,63 @@
+import { instruments, PrismaClient } from '@prisma/client'
+
 import React from 'react'
 
-const Sales = () => {
-  return <div>Sales</div>
+export async function getStaticProps() {
+  const prisma = new PrismaClient()
+  const instruments = await prisma.instruments.findMany({
+    where: {
+      sold: "false"
+    }
+  })
+  return {
+    props: { instruments }
+  }
+}
+
+type InstrumentProps = {
+  id: number
+  maker: string
+  sku: number
+  description: string
+  origin: string
+  sounds_like: string
+  date_made: string
+  image_path: string
+  audio_path: string
+  sold: string
+}
+
+
+const Sales: React.FC<{ instruments: InstrumentProps[] }> = ({ instruments }) => {
+  console.log(instruments)
+
+  return (
+    <div>
+      <h3>
+        Instruments for Sale
+      </h3>
+      <ul>
+        {instruments.map((i) => <li key={i.id}>
+          <div className="card">
+            <span>
+              {i.maker}
+            </span>
+            <span>
+              {i.origin}
+            </span>
+            <span>
+              {i.date_made}
+            </span>
+            <span>
+              {i.sold}
+            </span>
+          </div>
+        </li>)}
+      </ul>
+
+    </div>
+  )
+
 }
 
 export default Sales
